2903e0b46e5f465e25af793b128bfad24bc6f67d
Kiran3581 <117075076+Kiran3581@users.noreply.github.com>
2022-11-24 08:54:15 +0530

    Create formula2.js
    maths formula in js

---
+++ Javascript/js/formula2.js
@@ -0,0 +1,11 @@
+function add() {
+    var a= document.getElementById('num1').value;
+    var b= document.getElementById('num2').value;
+    
+    var res=(a-b)*(a+b);
+    
+    console.log(res);
+    }
+
+
+    // a2 – b2 = (a – b)(a + b)
